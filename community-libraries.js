ed6ea6244fe0a2f6dd2e2eeeca66b0fa0790c834
John Pham <mr.john.pham@gmail.com>
2020-07-06 11:36:52 -0400

    Rewrite getLibraryZIPCode as async function

--- community-libraries.js
+++ community-libraries.js
@@ -4,21 +4,29 @@ https://data.cityofnewyork.us/resource/b67a-vkqb.json?name=Arverne&$$apptoken=Qo
           
 var schoolsInZIPCode = 0;
 
-function getLibraryZIPCode(libraryName) { //given a library's name, return the ZIP code
-    var ZIPCode = 0;
+async function getLibraryZIPCode(libraryName) { //given a library's name, return the ZIP code
+    // var ZIPCode = 0;
     var buildURL = "https://data.cityofnewyork.us/resource/b67a-vkqb.json?name=" + libraryName; //build a URL that queries the selected library from the API endpoint
-    $.ajax({
-        url: buildURL,
-        async: false,
-        type: "GET",
-        data: {
-            "$limit" : 5000,
-            "$$app_token" : "QoQet97KEDYpMW4x4Manaflkp" //This is my (John Pham's) app token
-        },
-    }).done(function(data) {
-        ZIPCode = data[0]["postcode"];
-    });
-    return ZIPCode;
+    // $.ajax({
+    //     url: buildURL,
+    //     async: false,
+    //     type: "GET",
+    //     data: {
+    //         "$limit" : 5000,
+    //         "$$app_token" : "QoQet97KEDYpMW4x4Manaflkp" //This is my (John Pham's) app token
+    //     },
+    // }).done(function(data) {
+    //     ZIPCode = data[0]["postcode"];
+    // });
+
+    let response = await fetch(buildURL);
+    let data = await response.json();
+    //console.log(data[0]["postcode"]);
+    return data[0]["postcode"];
+        // .then(response => response.json())
+        // .then(data => console.log(data[0]["postcode"]));
+
+    // return ZIPCode;
 };
 
 function getNYCDOEPovertyRateByZIPCode(ZIPCode, datasetYear) {
@@ -159,7 +167,8 @@ $(document).ready(function(){
             fullLibraryName = shortLibraryName + " Community Library"; //generate the library's full name if it is not the Central Library
         }
 
-        var ZipCodeValue = getLibraryZIPCode(shortLibraryName); //Query the NYC DOE data to obtain the ZIP code.
+        //var ZipCodeValue = getLibraryZIPCode(shortLibraryName); //Query the NYC DOE data to obtain the ZIP code.
+        
         var NYCDOEPovertyRate = getNYCDOEPovertyRateByZIPCode(ZipCodeValue, NYCDOEDataset); //Query the NYC DOE data to obtain the student poverty percentage.
         var unemploymentRate = getAmericanCommunitySurvey5YearEstimateValue(ACSdataset,"S2301",ZipCodeValue);
         var percentageNoHSDiploma = getAmericanCommunitySurvey5YearEstimateValue(ACSdataset,"S1501",ZipCodeValue); //S1501 is the American Community Survey table number for educational attainment
