793ff7394c39530da9f9f78c7c3fd5f83801cdb3
John Pham <mr.john.pham@gmail.com>
2020-07-08 13:12:29 -0400

    Delete unneeded comments

--- js/index.js
+++ js/index.js
@@ -8,20 +8,6 @@ function outputProfile(libraryData) { // output profile to #Profile, given the l
 
     $('#Profile').addClass("card");
     $('#Profile').html(`<div class="card-body">${fullLibraryName} is located in ZIP code ${zipCode}. According to the NYC Department of Education's ${nycDoeDataset} School Demographic Snapshot, ${nycDoePovertyRate}% of the students who attend the ${schoolsInZipCode} public school${schoolsInZipCode === 1 ? '' : 's'} located in this ZIP code receive free or reduced lunch or are eligible for NYC Human Resources Administration public benefits. According to the ${censusDataset} American Community Survey Five-Year Dataset, the ZIP code's unemployment rate is ${unemploymentRate}%; ${noHighSchoolDiplomaOrEquivalent}% of adults age 25 or older do not possess a high school diploma or its equivalent; ${censusPovertyRate}% of residents live below the poverty line; and ${limitedEnglishPercent}% of residents speak English less than very well.</div>`);
-
-    // let percentageNoHSDiploma = getAmericanCommunitySurvey5YearEstimateValue(ACSdataset,"S1501",zipCode); //S1501 is the American Community Survey table number for educational attainment
-
-    // $("#ACS1").append(" According to the American Community Survey ");
-    // $("#ACSdataset").append(ACSdataset);
-    // $("#ACS2").append(", the ZIP code's unemployment rate is ");
-    // $("#Unemployment").append(unemploymentRate);
-    // $("#ACS3").append("%; ");
-    // $("#NoHS").append(percentageNoHSDiploma);
-    // $("#ACS4").append("% of residents do not possess a high school diploma or its equivalent; ");
-    // $("#ACSPoverty").append(ACSPovertyRate);
-    // $("#ACS5").append("% of residents live below the poverty line; and ");
-    // $("#LimitedEnglish").append(limitedEnglishProfiencyRate);
-    // $("#ACS6").append("% of residents speak English less than very well."); 
 }
 
 $(document).ready(function(){
@@ -57,7 +43,6 @@ $(document).ready(function(){
                                 getLessThanHighSchoolDiploma(libraryData, function(err, libraryData) { // query the Census API to obtain high school educational attainment data
                                     if (err) console.log(`Error retrieving Census educational attainment data: ${err}`);
 
-                                    console.log(`Data is ${JSON.stringify(libraryData)}`);
                                     outputProfile(libraryData);
                                 })
                             })
