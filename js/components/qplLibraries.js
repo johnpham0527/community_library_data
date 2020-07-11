62f47577939605afd815a1a41d9d1ac3da380010
John Pham <mr.john.pham@gmail.com>
2020-07-11 09:15:55 -0400

    Fix shortLibraryName property assignment for allLibraryData

--- js/components/qplLibraries.js
+++ js/components/qplLibraries.js
@@ -95,35 +95,10 @@ const getAllLibraries = async () => {
 
         console.log(`libraryData is ${JSON.stringify(libraryData)}`);
 
-        allLibraryData.shortLibraryName = libraryData;   
+        allLibraryData[shortLibraryName] = libraryData;   
     }
-    // qplLibraries.forEach(async shortLibraryName => { //for each library in the qplLibraries array
-    //     let libraryData = {
-    //         schoolsInZipCode: 0, // variable for counting number of schools in a ZIP code
-    //         nycDoeDataset: '2018-2019',
-    //         censusDataset: '2018',
-    //         shortLibraryName: shortLibraryName,
-    //         fullLibraryName: shortLibraryName === 'Central Library' ? // generate each library's full name using a ternary operator
-    //             shortLibraryName : shortLibraryName + ' Community Library' // generate the library's full name if it is not the Central Library
-    //     }
     
-    //     libraryData = await getLibraryZipCode(libraryData) // query the NYC DOE data to obtain the ZIP code.
-    //     .then(async zipCode => {
-    //         libraryData.zipCode = zipCode;
-    
-    //         return await getNycDoePoverty(libraryData) //assign this promise chain to libraryData object
-    //             .then(libraryData => getCensusPoverty(libraryData))
-    //             .then(libraryData => getUnemployment(libraryData))
-    //             .then(libraryData => getLimitedEnglishProficiency(libraryData))
-    //             .then(libraryData => getLessThanHighSchoolDiploma(libraryData))
-    //     })
-        
-    //     console.log(`libraryData is ${libraryData}`);
-
-    //     allLibraryData.shortLibraryName = libraryData;
-    // })
-    
-    console.log(`allLibraryData is ${allLibraryData}`);
+    console.log(`allLibraryData is ${JSON.stringify(allLibraryData)}`);
 
     return allLibraryData;
 }
