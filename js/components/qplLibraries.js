9a057f6b81c47a7230241e373b6913df2ffd7930
John Pham <mr.john.pham@gmail.com>
2020-07-11 08:41:25 -0400

    Add secondary button: view all libraries

--- js/components/qplLibraries.js
+++ js/components/qplLibraries.js
@@ -64,3 +64,31 @@ const qplLibraries = [
     'Woodside'
 ]
 
+let allLibraryData = {}; // this object will store a hash map of libraryData objects for each QPL community library
+
+qplLibraries.forEach(shortLibraryName => { //for each library in the qplLibraries array
+    let libraryData = {
+        schoolsInZipCode: 0, // variable for counting number of schools in a ZIP code
+        nycDoeDataset: '2018-2019',
+        censusDataset: '2018',
+        shortLibraryName: shortLibraryName,
+        fullLibraryName: shortLibraryName === 'Central Library' ? // generate each library's full name using a ternary operator
+            shortLibraryName : shortLibraryName + ' Community Library' // generate the library's full name if it is not the Central Library
+    }
+
+    getLibraryZipCode(libraryData) // query the NYC DOE data to obtain the ZIP code.
+    .then(async zipCode => {
+        libraryData.zipCode = zipCode;
+
+        return await getNycDoePoverty(libraryData) //assign this promise chain to libraryData object
+            .then(libraryData => getCensusPoverty(libraryData))
+            .then(libraryData => getUnemployment(libraryData))
+            .then(libraryData => getLimitedEnglishProficiency(libraryData))
+            .then(libraryData => getLessThanHighSchoolDiploma(libraryData))
+            .then(libraryData => outputProfile(libraryData));
+    })
+
+    allLibraryData.shortLibraryName = libraryData;
+})
+
+console.log(`allLibraryData is ${allLibraryData}`);
